89462f4e3d1caaefe15221c5a7c84b06e2a095dc
NathanLovell <nslovell@mail.csuchico.edu>
2019-12-13 19:57:38 -0800

    test for each function to make sure tests are set up correctly

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,35 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
-	protected:
-		PiezasTest(){} //constructor runs before each test
-		virtual ~PiezasTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+protected:
+	PiezasTest() {}			   //constructor runs before each test
+	virtual ~PiezasTest() {}   //destructor cleans up after tests
+	virtual void SetUp() {}	//sets up before each test (after constructor)
+	virtual void TearDown() {} //clean up after each test, (before destructor)
 };
 
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, dropPiece)
+{
+	Piezas board;
+	ASSERT_EQ(board.dropPiece(1), X);
+}
+
+TEST(PiezasTest, pieceAt)
+{
+	Piezas board;
+	ASSERT_EQ(board.pieceAt(1, 5), Invalid);
+}
+
+TEST(PiezasTest, gameState)
+{
+	Piezas board;
+	ASSERT_EQ(board.gameState(), Invalid);
 }
